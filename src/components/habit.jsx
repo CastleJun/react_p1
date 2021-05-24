8af62ce541a2c1bfd83d4b13598e1bc2ec964613
SJ <hsj9360@gmail.com>
2021-05-25 00:54:51 +0900

    Habits Components State up, list key

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -13,10 +13,12 @@ class Habit extends Component {
         this.setState({count: this.state.count +1 });
     }
     render() {
+        console.log(this.props.habit);
+        const { name, count } = this.props.habit;
         return (
             <li className="habit">
-            <span className="habit-name">Reading</span>
-            <span className="habit-count">{this.state.count}</span>
+            <span className="habit-name">{name}</span>
+            <span className="habit-count">{count}</span>
             <button 
                 className="habit-button habit-increase" 
                 onClick={this.handleIncrement}
