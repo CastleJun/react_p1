4b65d0095103efe9dcbcf7e22d228127e9cda62b
SJ <hsj9360@gmail.com>
2021-05-31 01:26:44 +0900

    event control & reset button

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-    state = {
-        count: 0
-    };
+    
     handleDecreament = () =>{
-        const count = this.state.count - 1;
-        this.setState({count: count < 0 ? 0 : count});
+        this.props.onDecrement(this.props.habit);
     }
     handleIncrement = () =>{
-        //state 오브젝트 안에 있는 ㅊount를 증가 한뒤 state를 업데이트 해야 함.
-        this.setState({count: this.state.count +1 });
+        this.props.onIncrement(this.props.habit);
     }
+
+    handleDelete = () => {
+        this.props.onDelete(this.props.habit);
+    };
+
     render() {
-        console.log(this.props.habit);
         const { name, count } = this.props.habit;
         return (
             <li className="habit">
@@ -31,7 +31,8 @@ class Habit extends Component {
             >
                 <i className="fas fa-minus-square"></i>
             </button>
-            <button className="habit-button habit-delete">
+            <button className="habit-button habit-delete"
+            onClick={this.handleDelete}>
                 <i className="fas fa-trash"></i>
             </button>
             </li>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-    state = {
-        habits: [
-            {id: 1, name: 'Reading', count: 0},
-            {id: 2, name: 'Running', count: 0},
-            {id: 3, name: 'Coding', count: 0},
-        ],
+    
+// state 를 수정하는것은 좋지 않다. 
+    handleIncrement = habit =>{
+        this.props.onIncrement(habit);
+    }
+    handleDecreament = habit =>{
+        this.props.onDecrement(habit);
+    }
+
+    handleDelete = habit => {
+        this.props.onDecrement(habit);
     };
+    handleAdd = name => {
+        this.props.onAdd(name);
+    }
+
     render() {
         return (
+            <>
+            <HabitAddForm onAdd={this.handleAdd} />
             <ul>
-                {this.state.habits.map(habit => (
-                    <Habit key={habit.id} habit={habit}/>
+                {this.props.habits.map(habit => (
+                    <Habit 
+                    key={habit.id}
+                    habit={habit} 
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecreament}
+                    onDelete={this.handleDelete}
+                    />
                 ))}
             </ul>
+            <button className="hab-ts-reset" onClick={this.props.onReset}>Reset All</button>
+            </>
         );
     }
 }
